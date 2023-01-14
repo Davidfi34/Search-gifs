ba16e83e9d5825b44b127e35713f5524a72f8c7d
David <figuererodavid@gmail.com>
2023-01-14 17:06:27 -0300

    http requests

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
   styleUrls: ['./busqueda.component.css']
 })
-export class BusquedaComponent implements OnInit {
 
-  constructor() { }
+export class BusquedaComponent {
+  
+  @ViewChild('txtbuscar') txtbuscar!:ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
+  constructor( private gifsService: GifsService ){}
+
+  buscar(){
+    
+    const valor = this.txtbuscar.nativeElement.value;  
+    if(valor.trim().length === 0 ){
+      return;
+    }
+    this.gifsService.BuscarGifs( valor );
+    this.txtbuscar.nativeElement.value = '';  
   }
+
 
 }
